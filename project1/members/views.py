9a78d380dd164afca5509ecbfff7335a26b424c2
x2Zerotwoz <papon1234xd@gmail.com>
2024-09-18 12:26:22 +0700

    update

--- project1/members/views.py
+++ project1/members/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-    template = loader.get_template('members.html')
-    return HttpResponse(template.render())
-
+    members = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'members': members,
+  }
+  return HttpResponse(template.render(context, request))
